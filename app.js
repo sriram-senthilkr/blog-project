dbf00515f21675f0d79f3eec5e6d1c449b563741
Sriram Senthil <sriram.senthilkr@gmail.com>
2023-01-20 13:57:44 +0800

    Each post redirects to its own page upon search

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
 	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -40,7 +41,18 @@ app.get("/compose", (req, res) => {
 });
 
 app.get("/posts/:postName", (req, res) => {
-	console.log(req.params.postName);
+	const requiredPost = _.kebabCase(req.params.postName);
+
+	journal.forEach(function (post) {
+		const availablePosts = _.kebabCase(post.title);
+
+		if (availablePosts === requiredPost) {
+			res.render("post", {
+				postTitle: availablePosts,
+				postBody: post.content,
+			});
+		}
+	});
 });
 
 app.post("/compose", (req, res) => {
